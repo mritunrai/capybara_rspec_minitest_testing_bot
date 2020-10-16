d9198f613f33243bc39de6ed78cdcd5ba454d6b8
Mritunjai Rai <mkrai@MKRai.local>
2020-10-16 15:56:30 +0530

    added test code

--- spec/github_homepage.rb
+++ spec/github_homepage.rb
@@ -2,22 +2,14 @@ require 'capybara/rspec'
 
 require 'spec_helper'
 
-# describe "Google's Search Functionality" do
-#     it "can find search results" do
-#         # Capybara.register_driver :selenium_chrome do |app|
-#         #  Capybara::Selenium::Driver.new(app, browser: :chrome)
-#         #   end
-          
-#         # Capybara.javascript_driver = :selenium_chrome
-#         session.visit 'https://www.hotwire.com/checkout/#!/account/login'
-
-#     # session.visit 'https://www.hotwire.com/checkout/#!/account/login'
-#     # session.find('#sign-in-email').set(ENV.fetch('EMAIL'))
-#     # session.find(:xpath, '//input[@type="password"]').set(ENV.fetch('PASSWORD'))
-#     # session.find('.hw-btn-primary').click
-#     # session.all('.confirmation-code').each do |code|
-#     #   sleep 5
-#     #   expect(@driver.title).to eql("BrowserStack - Google Search")
-#     # end
-#   end
-# end
+describe "the signin process", type: :feature do
+    before :each do
+        @session = Capybara::Session.new(:chrome)
+        @session.visit("http://www.google.com") 
+    end
+   
+    it "can find search results" do
+            @session.fill_in 'q', with: 'user@example.com'
+            @session.click_button 'btnK'    
+    end
+end
